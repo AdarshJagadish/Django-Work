f11436faab18a9f25d79f3695dd8e093009ad06b
AdarshJagadish <jagadishadarsh2003@gmail.com>
2024-12-20 13:32:12 +0530

    initial commit

--- model_form/app/models.py
+++ model_form/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class teacher(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.CharField(max_length=30)
+    
+class media(models.Model):
+    file=models.FileField()
+    description=models.CharField(max_length=50)
--- model_form/app/views.py
+++ model_form/app/views.py
@@ -3,23 +3,22 @@ from .models import *
 from .form import *
 # Create your views here.
 
-def user_form_dis(request):
-    data=user_form()
-    if request.method == 'POST':
-        form=user_form(request.POST)
-        if form.is_valid():
-            roll=form.cleaned_data['roll']
-            name=form.cleaned_data['name']
-            mark=form.cleaned_data['mark']
-            data1=student.objects.create(roll=roll,name=name,mark=mark)
-            data1.save()
-    return render(request,'',{'data':data})
-
 def model_form_dis(request):
     data=model_form()
     if request.method=='POST':
         form=model_form(request.POST)
         form.save()
         return redirect(model_form_dis)
-    
-    return redirect(request,'index.html',{'data':data})
+    return render(request,'index.html',{'data':data})
+
+def uploadfile(request):
+    if request.method=='POST':
+        file=request.FILES['file']
+        description=request.POST['description']
+        data=media.objects.create(file=file,description=description)
+        # upload=media.objects.all()
+        # print(file,description)
+        # print(upload)
+        data.save()
+        return redirect(uploadfile)
+    return render(request,'index2.html')
