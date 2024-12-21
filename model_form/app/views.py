c1a919f26fec0b9bb4224ffc2ab0d1daad81a759
AdarshJagadish <jagadishadarsh2003@gmail.com>
2024-12-21 13:58:00 +0530

    initial commit

--- model_form/app/views.py
+++ model_form/app/views.py
@@ -12,13 +12,13 @@ def model_form_dis(request):
     return render(request,'index.html',{'data':data})
 
 def uploadfile(request):
+    photo=media.objects.all()
+    rev=reversed(list(photo))
+
     if request.method=='POST':
         file=request.FILES['file']
         description=request.POST['description']
         data=media.objects.create(file=file,description=description)
-        # upload=media.objects.all()
-        # print(file,description)
-        # print(upload)
         data.save()
         return redirect(uploadfile)
-    return render(request,'index2.html')
+    return render(request,'index2.html',{'photos':rev})
